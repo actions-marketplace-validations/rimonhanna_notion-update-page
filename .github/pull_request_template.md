ba2222fcd5fb676bf339a23175b64e5583caaa24
Rimon Hanna <rimon.hanna@live.com>
2022-08-22 08:54:10 +0200

    refactored code to take notion pageid as a parameter

--- .github/pull_request_template.md
+++
@@ -1,5 +0,0 @@
-[Notion link](notion_link_here)
-
-This PR adds... <!-- Brief description on what this PR does -->
-
-- [ ] Updated release version on README
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const axios = require("axios");
-const btoa = require("btoa");
 const core = require("@actions/core");
-const github = require("@actions/github");
 const { Client } = require("@notionhq/client");
 
 require("dotenv").config();
@@ -15,10 +12,6 @@ const SUPPORTED_EXISTING_VALUE_ACTIONS = {
   OVERWRITE: "overwrite",
   APPEND: "append",
 };
-
-const getGitHubRequestHeaders = (username, accessToken) => ({
-  headers: { Authorization: `Basic ${btoa(`${username}:${accessToken}`)}` },
-});
 
 const generateUpdateProps = (
   propertyType,
@@ -78,64 +71,12 @@ const updateNotionStory = async (
   });
 };
 
-const extractFirstNotionPageId = (prDescription) => {
-  const notionURLs = prDescription.match(
-    /(https?:\/\/)?(www\.)?notion\.so\/([A-Za-z0-9\-\/]+)/gi
-  );
-  if (notionURLs === null || notionURLs.length === 0) {
-    return null;
-  }
-  const firstNotionURLTokens = notionURLs[0].split("/");
-  const notionURLPath = firstNotionURLTokens[firstNotionURLTokens.length - 1];
-  const notionURLPathTokens = notionURLPath.split("-");
-  return notionURLPathTokens[notionURLPathTokens.length - 1];
-};
-
-const fetchPRDescription = async (prURL, username, accessToken) => {
-  const response = await axios.get(
-    prURL,
-    getGitHubRequestHeaders(username, accessToken)
-  );
-  const { body } = response.data;
-  return body;
-};
-
-const fetchPRURL = async (
-  commitHash,
-  username,
-  accessToken,
-  repoOwner,
-  repoName
-) => {
-  const response = await axios.get(
-    `https://api.github.com/search/issues?q=hash:${commitHash}`,
-    getGitHubRequestHeaders(username, accessToken)
-  );
-  const { items: issues } = response.data;
-
-  if (issues === null || issues.length === 0) {
-    return null;
-  }
-
-  const prURLRegex = new RegExp(
-    `https://api.github.com/repos/${repoOwner}/${repoName}/pulls/[0-9]+`
-  );
-  const { pull_request } = issues.find((issue) =>
-    issue.pull_request.url.match(prURLRegex)
-  );
-  return pull_request.url;
-};
-
 const getConfig = () => {
   const isOffline = process.env.NODE_ENV === "offline";
   if (isOffline) {
     return {
-      commitHash: process.env.COMMIT_HASH,
-      repoOwner: process.env.REPO_OWNER,
-      repoName: process.env.REPO_NAME,
-      username: process.env.GH_USERNAME,
-      accessToken: process.env.GH_ACCESS_TOKEN,
       notionKey: process.env.NOTION_KEY,
+      notionPageId: process.env.NOTION_PAGE_ID,
       notionPropertyName: process.env.NOTION_PROPERTY_NAME,
       notionPropertyType:
         process.env.NOTION_PROPERTY_TYPE || SUPPORTED_PROPERTY_TYPES.RICH_TEXT,
@@ -146,12 +87,8 @@ const getConfig = () => {
     };
   }
   return {
-    commitHash: github.context.sha,
-    repoOwner: github.context.repo.owner,
-    repoName: github.context.repo.repo,
-    username: core.getInput("gh-username"),
-    accessToken: core.getInput("gh-token"),
     notionKey: core.getInput("notion-key"),
+    notionPageId: core.getInput("notion-page-id"),
     notionPropertyName: core.getInput("notion-property-name"),
     notionPropertyType:
       core.getInput("notion-property-type") ||
@@ -165,12 +102,8 @@ const getConfig = () => {
 
 const run = async () => {
   const {
-    commitHash,
-    repoOwner,
-    repoName,
-    username,
-    accessToken,
     notionKey,
+    notionPageId,
     notionPropertyName,
     notionPropertyType,
     notionUpdateValue,
@@ -183,34 +116,6 @@ const run = async () => {
     core.setFailed(
       `Type of Notion Page property ${notionPropertyType} is not supported.`
     );
-  }
-
-  let prDescription = "";
-  try {
-    const prURL = await fetchPRURL(
-      commitHash,
-      username,
-      accessToken,
-      repoOwner,
-      repoName
-    );
-
-    if (prURL === null) {
-      core.setFailed(
-        `Error get PR URL for hash ${commitHash}. Commit may not be created by merging a PR.`
-      );
-    }
-
-    prDescription = await fetchPRDescription(prURL, username, accessToken);
-  } catch (error) {
-    core.setFailed(
-      `Error fetch PR description with hash ${commitHash}: ${error}`
-    );
-  }
-
-  const notionPageId = extractFirstNotionPageId(prDescription);
-  if (notionPageId === null) {
-    core.setFailed("No Notion URL found.");
   }
 
   try {
